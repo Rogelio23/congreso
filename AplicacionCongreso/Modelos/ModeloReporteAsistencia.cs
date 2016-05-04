51563e0625e43e036383b31ad84dd1937b6bc294
Gibran Amparan <GibranAmparanD@hotmail.com>
2016-05-04 14:36:43 -0700

    metiendo cambios para el modelo de reportes.

---
+++ AplicacionCongreso/Modelos/ModeloReporteAsistencia.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicacionCongreso.Modelos
+{
+    class ModeloReporteAsistencia
+    {
+        public String nombreAsistente { get; set; }
+        public List<String> eventos { get; set; }
+
+        public ModeloReporteAsistencia(){
+            this.eventos = new List<string>();
+        }
+
+        public ModeloReporteAsistencia(String nombreAsistente, String eventosConcatenadosPorComa)
+        {
+            String [] eventos = eventosConcatenadosPorComa.Split((",").ToCharArray());
+            this.eventos = new List<string>();
+            foreach (String evento in eventos)
+            {
+                this.eventos.Add(evento);
+            }
+            this.nombreAsistente = nombreAsistente;
+        }
+    }
+}
